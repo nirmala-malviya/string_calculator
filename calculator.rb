8383b1c2c358c306fbec5332b5bca3806ad1cc92
Nirmala Malviya <nirmla.malviya@gmail.com>
2024-02-28 23:47:10 +0530

    Changes for addition of multiple numbers

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,16 @@
 class Calculator
   def add(string_val)
     # Empty string check
-    return 0 if string_val.empty?       
+    return 0 if string_val.empty?
+
+    addition_val = 0
+    val_array = string_val.split(",")
+
+    val_array.each do |val|
+      val = val.to_i      
+      # Addition of numbers
+      addition_val = addition_val + val
+    end
+    addition_val       
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,15 @@ describe Calculator do
       calculator = Calculator.new
       expect(calculator.add("")).to eq(0)
     end
+
+    it 'returns the sum of a single numbers string' do
+      calculator = Calculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add("1,5")).to eq(6)
+    end
   end
 end
