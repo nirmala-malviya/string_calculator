47cf62cb6898b79c3904ed851fa4ad69409873e5
Nirmala Malviya <nirmla.malviya@gmail.com>
2024-02-29 00:03:41 +0530

    Added check for new line input string

--- calculator.rb
+++ calculator.rb
@@ -5,15 +5,17 @@ class Calculator
     # Empty string check
     return 0 if string_val.empty?
 
+    string_val = string_val.gsub(' ', '')
+    return "The following input is invalid: #{string_val}" if (string_val.include? (",\n")) || (string_val.include? ("\n,"))
+
     addition_val = 0
-    val_array = string_val.split(",")
+    # Input string split by new lines and commas
+    val_array = string_val.split(/[\n,]/)
 
     val_array.each do |val|
       val = val.to_i
-
       # Check for if number is negative
       raise NumberError.new(val) if val < 0
-
       # Addition of numbers
       addition_val = addition_val + val
     end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -19,7 +19,18 @@ describe Calculator do
 
     it 'raise the exception' do
       calculator = Calculator.new
+      # expect(calculator.add("1,-5")).to eq(6)
       expect { calculator.add("5, -3") }.to raise_error("Negative numbers not allowed: -3")
+    end
+
+    it 'return invalid input if string contain new line' do
+      calculator = Calculator.new
+      expect(calculator.add("1,\n")).to eq("The following input is invalid: 1,\n")
+    end
+
+    it 'return invalid input if string contain blank space' do
+      calculator = Calculator.new
+      expect(calculator.add("1, \n")).to eq("The following input is invalid: 1,\n")
     end
   end
 end
